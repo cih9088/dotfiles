4e116c95a0d473ffbb8a866e1a9b66ba2e7764aa
Inhyuk Jo <cih9088@gmail.com>
2016-08-28 20:55:53 +0900

    Initial Commit

---
+++ tmux_setup.sh
@@ -0,0 +1,30 @@
+VERSION=2.2
+
+if [[ $1 = local ]]; then
+    echo 'Build "libevent-dev" and "libncurses-dev".' > &2
+else
+    sudo apt-get -y install libevent-dev libncurses-dev
+    sudo apt-get -y remove tmux
+fi
+
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+extract tmux-${VERSION}.tar.gz
+rm -f tmux-${VERSION}.tar.gz
+cd tmux-${VERSION}
+
+if [[ $1 = local ]]; then
+    ./configure --prefix=$HOME/local
+    make
+    make install
+    mkdir -p $HOME/local/src
+    cd -
+    rm -rf $HOME/local/src/tmux-*
+    mv tmux-${VERSION} $HOME/local/src
+else
+    ./configure
+    make
+    sudo make install
+    cd -
+    sudo rm -rf /usr/local/src/tmux-*
+    sudo mv tmux-${VERSION} /usr/loca/src
+fi
