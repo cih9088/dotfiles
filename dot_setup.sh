763e056aa7088e2c2dbb2d8063541d1988d1199c
Inhyuk Jo <cih9088@gmail.com>
2016-08-28 22:46:43 +0900

    Initial commit

---
+++ dot_setup.sh
@@ -0,0 +1,12 @@
+# backup old files
+mkdir ~/dotfiles_old
+cp ~/.vimrc ~/dotfiles_old
+cp ~/.tmux.config ~/dotfiles_old
+cp ~/.zshrc ~/dotfiles_old
+cp ~/.zpreztorc ~/dotfiles_old
+
+# Copy dot files
+\cp vimrc ~/.vimrc
+\cp tmux.config ~/.tmux.config
+\cp zshrc ~/.zshrc
+\cp zpreztorc ~/.zpreztorc
---
+++ vim_setup.sh
@@ -0,0 +1,5 @@
+# Clone vundle repository
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install Plugins from command line
+vim +PluginInstall +qall
