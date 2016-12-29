93636a9ebf3780d40b82ba42d6101b0bd72f3e1d
Inhyuk Jo <ihcho@postech.ac.kr>
2016-12-29 02:12:55 -0500

    tar change, typo fixed

--- tmux_setup.sh
+++ tmux_setup.sh
@@ -9,7 +9,7 @@ else
 fi
 
 wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
-extract tmux-${VERSION}.tar.gz
+tar -xvzf tmux-${VERSION}.tar.gz
 rm -f tmux-${VERSION}.tar.gz
 cd tmux-${VERSION}
 
@@ -27,5 +27,5 @@ else
     sudo make install
     cd -
     sudo rm -rf /usr/local/src/tmux-*
-    sudo mv tmux-${VERSION} /usr/loca/src
+    sudo mv tmux-${VERSION} /usr/local/src
 fi
