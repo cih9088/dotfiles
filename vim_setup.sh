c52da44bd47f89f9c38c90efdb9a6c455e3cbfaa
Inhyuk Jo <ihcho@postech.ac.kr>
2017-03-12 00:50:10 +0900

    vim-plug auto install is merged to vimrc file

--- vim_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-# Clone vim-plug repository
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Install Plugins from command line
-vim +PlugInstall +qall
