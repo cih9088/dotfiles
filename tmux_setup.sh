0041af704f08fa19cc2a6ce0814c107c58941c90
Inhyuk Jo <ihcho@postech.ac.kr>
2017-03-23 02:24:55 +0900

    add mac support

--- tmux_setup.sh
+++ tmux_setup.sh
@@ -1,11 +1,33 @@
 #!/bin/zsh
+
+# exit on error
+set -e
+
 VERSION=2.3
+
+case "$OSTYPE" in
+    solaris*) platform='SOLARIS' ;;
+    darwin*)  platform='OSX' ;; 
+    linux*)   platform='LINUX' ;;
+    bsd*)     platform='BSD' ;;
+    msys*)    platform='WINDOWS' ;;
+    *)        platform='unknown: $OSTYPE' ;;
+esac
+
 
 if [[ $1 = local ]]; then
     echo 'Build "libevent-dev" and "libncurses-dev".' >&2
 else
-    sudo apt-get -y install libevent-dev libncurses-dev
-    sudo apt-get -y remove tmux
+    if [[ $platform == "LINUX" ]]; then
+        sudo apt-get -y install libevent-dev libncurses-dev
+        sudo apt-get -y remove tmux
+    elif [[ $platform == "OSX" ]]; then
+        brew install libevent ncurses
+        brew install wget
+        brew uninstall tmux
+    else
+        print 'Not defined'
+    fi
 fi
 
 wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
