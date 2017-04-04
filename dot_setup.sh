6800f6f21be85be4f64da716683d2e9890bb051d
Inhyuk Jo <ihcho@postech.ac.kr>
2017-04-04 15:44:03 +0900

    tmux configuration

--- dot_setup.sh
+++ dot_setup.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 # backup old files
-mkdir ~/dotfiles_old
-cp ~/.vimrc ~/dotfiles_old
-cp ~/.tmux.conf ~/dotfiles_old
-cp ~/.zshrc ~/dotfiles_old
-cp ~/.zpreztorc ~/dotfiles_old
+\mkdir ~/dotfiles_old
+\cp ~/.vimrc ~/dotfiles_old
+\cp ~/.tmux.conf ~/dotfiles_old
+\cp ~/.zshrc ~/dotfiles_old
+\cp ~/.zpreztorc ~/dotfiles_old
 
 # Copy dot files
-\cp vimrc ~/.vimrc
-\cp tmux.conf ~/.tmux.conf
-\cp zshrc ~/.zshrc
-\cp zpreztorc ~/.zpreztorc
+\cd ~
+\ln -s -f dotfiles/vimrc ~/.vimrc
+\ln -s -f dotfiles/tmux.conf ~/.tmux.conf
+\ln -s -f dotfiles/tmux.conf.local ~/.tmux.conf.local
+\ln -s -f dotfiles/zshrc ~/.zshrc
+\ln -s -f dotfiles/zpreztorc ~/.zpreztorc
