d6779d4a88f84ace5f6ebe3ba5d3f2631eb1dad3
mlg <ihcho@postech.ac.kr>
2017-06-28 00:07:44 +0900

    no need garrett prompt

--- prezto_setup.sh
+++ prezto_setup.sh
@@ -3,7 +3,8 @@
 # htts://github.com/sorin-ionescu/prezto
 
 # Clone prezto the repository
-git clone --recursive https://github.com/cih9088/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+#git clone --recursive https://github.com/cih9088/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 # Create a new zsh configureation by copying the zsh config files
 setopt EXTENDED_GLOB
@@ -11,11 +12,11 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-# Clone garrett prompt repository
-git clone https://github.com/cih9088/zsh-prompt-garrett.git ./prompt
-
-cd prompt
-cp prompt_garrett_setup ~/.zprezto/modules/prompt/functions/
-cd ..
-rm -rf prompt
+## Clone garrett prompt repository
+#git clone https://github.com/cih9088/zsh-prompt-garrett.git ./prompt
+#
+#cd prompt
+#cp prompt_garrett_setup ~/.zprezto/modules/prompt/functions/
+#cd ..
+#rm -rf prompt
 
