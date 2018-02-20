ca71c40b8e505584bc4a0c508acd4dd258881729
Inhyuk Jo <cih9088@gmail.com>
2018-02-20 15:04:11 +0900

    minor error fix

--- dot_setup.sh
+++ dot_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ -d "~/dotfiles_old" ]; then
+PROJ_HOME="$(pwd)"
+if [ -d "${HOME}/dotfiles_old" ]; then
     echo "dotfiles_old folder already exists"
     exit
 fi
@@ -15,14 +16,13 @@ fi
 
 # make directory if needed
 if [ ! -d "~/.config/nvim" ]; then
-    \mkdir ~/.config/nvim
+    \mkdir -p ~/.config/nvim
 fi
 
 # Copy dot files
-\cd ~
-\ln -s -f dotfiles/vimrc ~/.vimrc
-\ln -s -f dotfiles/vimrc ~/.config/nvim/init.vim
-\ln -s -f dotfiles/tmux.conf ~/.tmux.conf
-\ln -s -f dotfiles/tmux.conf.local ~/.tmux.conf.local
-\ln -s -f dotfiles/zshrc ~/.zshrc
-\ln -s -f dotfiles/zpreztorc ~/.zpreztorc
+\ln -s -f ${PROJ_HOME}/vimrc ~/.vimrc
+\ln -s -f ${PROJ_HOME}/vimrc ~/.config/nvim/init.vim
+\ln -s -f ${PROJ_HOME}/tmux.conf ~/.tmux.conf
+\ln -s -f ${PROJ_HOME}/tmux.conf.local ~/.tmux.conf.local
+\ln -s -f ${PROJ_HOME}/zshrc ~/.zshrc
+\ln -s -f ${PROJ_HOME}/zpreztorc ~/.zpreztorc
