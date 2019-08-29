0a21b736a986aaaf6c16ce228469fb6e606b6b17
Andy <cih9088@gmail.com>
2019-08-29 17:49:51 +0900

    change key mapping of skhd

---
+++ config/bitbar/skhd_mode.1h.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH=/usr/local/bin:$PATH
+
+# skhd saves the current keyboard mode to this file
+modefile=$TMPDIR/current_skhd_mode
+if [[ -r $modefile ]]; then
+    mod="$(cat "$modefile")"
+    mod="$(echo "$mod" | awk '{print toupper($0)}')"
+    if [[ -n $mod ]]; then
+        skhd_mode="$mod"
+        echo -e "$skhd_mode | \
+            ansi=true \
+            dropdown=false \
+            trim=false color=#bf616a"
+    fi
+fi
