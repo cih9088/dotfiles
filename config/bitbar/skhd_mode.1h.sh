746d91d19ce069c7f1599b7a0c3183accbd57700
Andy <cih9088@gmail.com>
2019-11-05 00:26:19 +0900

    try new colorscheme

--- config/bitbar/skhd_mode.1h.sh
+++ config/bitbar/skhd_mode.1h.sh
@@ -9,9 +9,26 @@ if [[ -r $modefile ]]; then
     mod="$(echo "$mod" | awk '{print toupper($0)}')"
     if [[ -n $mod ]]; then
         skhd_mode="$mod"
-        echo -e "$skhd_mode | \
-            ansi=true \
-            dropdown=false \
-            trim=false color=#bf616a"
+        if [[ $skhd_mode == 'FOCUS' ]]; then
+            echo ":exclamation:"
+            echo "---"
+            echo "Focus mode"
+        elif [[ $skhd_mode == 'WARP' ]]; then
+            echo ":collision:"
+            echo "---"
+            echo "Warp mode"
+        elif [[ $skhd_mode == 'RESIZE' ]]; then
+            echo ":point_right:"
+            echo "---"
+            echo "Resize mode"
+        elif [[ $skhd_mode == 'PREFIX' ]]; then
+            echo ":metal:"
+            echo "---"
+            echo "Prefix mode"
+        fi
+    else
+        echo ":relieved:"
+        echo "---"
+        echo "Normal mode"
     fi
 fi
