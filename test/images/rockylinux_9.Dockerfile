c4083546b14da5442f54518236a3880bab1d3e85
Inhyuk Andy Cho <cih9088@gmail.com>
2023-04-11 09:12:56 +0900

    test: make it faster

--- test/images/rockylinux_9.Dockerfile
+++
@@ -1,7 +0,0 @@
-from rockylinux:9
-
-RUN dnf install -y sudo git make passwd && dnf clean all && rm -rf /var/cache/dnf
-
-RUN passwd -d root
-RUN useradd -m -s /bin/bash docker && passwd -d docker && usermod docker -G wheel
-USER docker
---
+++ tests/run.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+
+
+# git config --global --add safe.directory /home/docker/dotfiles; sudo chown -R docker.docker /home/docker/dotfiles
+# /home/docker/dotfiles/bin/dots --yes --verbose --mode system --config /home/docker/dotfiles/config.yaml install
+# /home/docker/dotfiles/bin/dots --yes --verbose --mode local --config /home/docker/dotfiles/config.yaml install
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+ERR_CODE_INSTALL=111
+ERR_CODE_REMOVE=222
+
+DEFAULT_CMD="docker"
+if command -v podman > /dev/null; then
+  DEFAULT_CMD="podman"
+fi
+
+COMMAND="${COMMAND:-$DEFAULT_CMD}"
+N_WORKERS="${N_WORKERS:-$(( $(nproc --all) / 2 ))}"
+WORK_DIR=""
+
+[ "${N_WORKERS}" -le 0 ] && N_WORKERS=1
+
+if [ "${N_WORKERS}" -gt 1 ]; then
+  WORK_DIR=$(mktemp -d)
+  echo "${WORK_DIR}"
+fi
+
+FAILED=()
+
+IMAGES=("rockylinux:dot" "ubuntu:dot")
+MODES=("local" "system")
+ITEMS=(
+  perl "asdf perl"
+  "pyenv python" "asdf python python-env"
+  "goenv golang" "asdf golang"
+  "asdf rust"
+  nodejs "asdf nodejs nodejs-env"
+  "asdf lua lua-env"
+  sh-env
+
+  terminfo cmake zlib bzip2 unzip gzip xz libjpeg-turbo opencv ncurses
+  "zsh prezto" neovim "tmux tpm"
+  wget tree fd rg thefuck tldr bash-snippets up jq sox tcpdump
+
+  bin configs
+)
+
+
+open_sem(){
+  mkfifo pipe-$$
+  exec 3<>pipe-$$
+  rm pipe-$$
+  local i=$1
+  for((;i>0;i--)); do
+    printf "%s\n" 0 >&3
+  done
+}
+
+test_return() {
+  local x="$1"
+  local exit_code
+  local cmd
+
+  exit_code=$(echo "$x" | cut -d ' ' -f 1)
+  cmd=$(echo "$x" | cut -d ' ' -f 3-)
+
+  if [ "${exit_code}" != 0 ]; then
+    if [ "${exit_code}" = ${ERR_CODE_INSTALL} ]; then
+      FAILED+=("INSTALL $cmd")
+    elif [ "${exit_code}" = ${ERR_CODE_REMOVE} ]; then
+      FAILED+=("REMOVE $cmd")
+    else
+      FAILED+=("UNKNOWN $cmd")
+    fi
+  fi
+}
+
+run_with_lock(){
+  local x
+
+  echo "$@"
+
+  read -u 3 -r x
+  test_return "$x"
+  (
+  if [ ${N_WORKERS} -gt 1 ]; then
+    ( "$@"; )  &>"$WORK_DIR/${image}_${mode}_${items}"
+  else
+    ( "$@"; )
+  fi
+  echo "$? $@" >&3
+  )&
+}
+
+task() {
+  local image=$1
+  local mode=$2
+  local items=$3
+
+  local exit_code=0
+
+  ${COMMAND} run --rm \
+    -v "$DIR"/..:/dotfiles:z \
+    "${image}" \
+    bash -c "\
+      sudo cp -r /dotfiles /home/docker/dotfiles &&
+      sudo chown -R docker.docker /home/docker/dotfiles &&
+      git config --global --add safe.directory /home/docker/dotfiles;
+      EXIT_CODE=0;
+      /home/docker/dotfiles/bin/dots \
+        --verbose --yes --mode ${mode} install ${items[*]} || EXIT_CODE=${ERR_CODE_INSTALL};
+      [ \$EXIT_CODE != 0 ] && exit \$EXIT_CODE;
+      /home/docker/dotfiles/bin/dots \
+        --verbose --yes --mode ${mode} remove $(echo "${items}" | tr " " "\n" | tac | tr "\n" " ") || EXIT_CODE=${ERR_CODE_REMOVE};
+      exit \$EXIT_CODE;
+      " || exit_code=$?
+
+  return $exit_code
+}
+
+open_sem "${N_WORKERS}"
+for image in "${IMAGES[@]}"; do
+  for items in "${ITEMS[@]}"; do
+    for mode in "${MODES[@]}"; do
+      run_with_lock task "${image}" "${mode}" "${items}"
+    done
+  done
+done
+wait
+
+for((i=$N_WORKERS;i>0;i--)); do
+  read -u 3 -t 1 -r x
+  test_return "$x"
+done
+
+if [ ${#FAILED[@]} -gt 0 ]; then
+  echo
+  echo "FAILED"
+  for i in "${FAILED[@]}"; do
+    echo "$i"
+  done
+fi
