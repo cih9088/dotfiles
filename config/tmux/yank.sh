28ffb271c0f72a4a0223bcb6e62eb0f4931eb93b
Inhyuk Andy Cho <cih9088@gmail.com>
2023-05-08 13:00:45 +0900

    fix: copy using osc52

--- config/tmux/yank.sh
+++ config/tmux/yank.sh
@@ -19,34 +19,37 @@ buf=$(cat "$@")
 eval "$(tmux show-env -s)"
 
 # try osc52 first
+osc52_failed=false
 if is_app_installed osc52; then
-    printf "%s" "$buf" | osc52
+  printf "%s" "$buf" | osc52 || osc52_failed=true
 fi
 
-# fallback to other solutions
-if is_app_installed pbcopy; then
+if [ "$osc52_failed" = "true" ]; then
+  # fallback to other solutions
+  if is_app_installed pbcopy; then
     # OSX
     copy_backend="pbcopy"
-elif is_app_installed reattach-to-user-namespace; then
+  elif is_app_installed reattach-to-user-namespace; then
     # OSX
     copy_backend="reattach-to-user-namespace pbcopy"
-elif [ -n "${DISPLAY-}" ] && is_app_installed xsel; then
+  elif [ -n "${DISPLAY-}" ] && is_app_installed xsel; then
     # Linux X11 solution
     copy_backend="xsel -i --clipboard"
-elif [ -n "${DISPLAY-}" ] && is_app_installed xclip; then
+  elif [ -n "${DISPLAY-}" ] && is_app_installed xclip; then
     # Linux X11 solution
     copy_backend="xclip -i -f -selection primary | xclip -i -selection clipboard"
-elif is_app_installed clip.exe; then
+  elif is_app_installed clip.exe; then
     # Windows
     copy_backend="clip.exe"
-# fallback to ssh tunnel
-elif [ -n "${copy_backend_remote_tunnel_port-}" ] \
-    && (netstat -f inet -nl 2>/dev/null || netstat -4 -nl 2>/dev/null) \
-    | grep -q "[.:]$copy_backend_remote_tunnel_port"; then
+  # fallback to ssh tunnel
+  elif [ -n "${copy_backend_remote_tunnel_port-}" ] \
+      && (netstat -f inet -nl 2>/dev/null || netstat -4 -nl 2>/dev/null) \
+      | grep -q "[.:]$copy_backend_remote_tunnel_port"; then
     copy_backend="nc localhost $copy_backend_remote_tunnel_port"
-fi
+  fi
 
-# if copy backend is resolved, copy and exit
-if [ -n "$copy_backend" ]; then
+  # if copy backend is resolved, copy and exit
+  if [ -n "$copy_backend" ]; then
     printf "%s" "$buf" | eval "$copy_backend"
+  fi
 fi
