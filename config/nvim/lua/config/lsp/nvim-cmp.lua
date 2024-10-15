123ca9bd8fbcaf8e6b6ef40fbe4290c87847ad85
Inhyuk Andy Cho <cih9088@gmail.com>
2024-10-15 09:57:56 +0900

    feat: add underscore comparator

--- config/nvim/lua/config/lsp/nvim-cmp.lua
+++ config/nvim/lua/config/lsp/nvim-cmp.lua
@@ -9,6 +9,18 @@ local function luasnip_setup()
    -- require("luasnip.loaders.from_vscode").lazy_load()
 end
 
+-- https://github.com/lukas-reineke/cmp-under-comparator
+local function underscore(entry1, entry2)
+   local _, entry1_under = entry1.completion_item.label:find "^_+"
+   local _, entry2_under = entry2.completion_item.label:find "^_+"
+   entry1_under = entry1_under or 0
+   entry2_under = entry2_under or 0
+   if entry1_under > entry2_under then
+      return false
+   elseif entry1_under < entry2_under then
+      return true
+   end
+end
 
 local function cmp_setup()
    local has_words_before = function()
@@ -86,6 +98,7 @@ local function cmp_setup()
             cmp.config.compare.offset,
             cmp.config.compare.exact,
             cmp.config.compare.score,
+            underscore,
             cmp.config.compare.recently_used,
             cmp.config.compare.kind,
          },
