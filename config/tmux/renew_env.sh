1ef9fda8d009c1d3bce536cde2938d9cf296dba0
Inhyuk Andy Cho <cih9088@gmail.com>
2025-09-18 12:34:53 +0900

    feat: add ghostty

--- config/tmux/renew_env.sh
+++ config/tmux/renew_env.sh
@@ -6,7 +6,20 @@ pane_fmt="#{pane_id} #{pane_in_mode} #{pane_input_off} #{pane_dead} #{pane_curre
 tmux list-panes -s -F "$pane_fmt" | awk '
   $2 == 0 && $3 == 0 && $4 == 0 && $5 ~ /(bash|zsh|ksh|fish)/ { print $1 }
 ' | while read -r pane_id; do
+
   # renew environment variables according to update-environment tmux option
-  # also clear screen
-  tmux send-keys -t "$pane_id" 'Enter' 'eval "$(tmux show-env -s)"' 'Enter'
+  _CMD=' eval "$(tmux show-env -s)"; '
+
+  # renew ls color
+  [ -f $DOTS/config/zsh/ls_colors/ls_colors.$COLOUR_SCHEME ] &&
+    _CMD+='export LS_COLORS="$(cat $DOTS/config/zsh/ls_colors/ls_colors.$COLOUR_SCHEME)"; ' ||
+    _CMD+='export LS_COLORS="$(cat $DOTS/config/zsh/ls_colors/ls_colors.$BG_LUMINANCE)"; '
+
+  # renew fzf color
+  [ -f $DOTS/config/zsh/fzf_colors/fzf.$COLOUR_SCHEME ] &&
+    _CMD+='export FZF_DEFAULT_OPTS="$(cat $DOTS/config/zsh/fzf_colors/fzf.$COLOUR_SCHEME)"; ' ||
+    _CMD+='export FZF_DEFAULT_OPTS="$(cat $DOTS/config/zsh/fzf_colors/fzf.$BG_LUMINANCE)"; '
+
+  # Execute
+  tmux send-keys -t "$pane_id" 'Enter' "$_CMD" 'Enter'
 done;
