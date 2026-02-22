2bc8b4db74365d952bbc0154eb8e7f8e8662821c
Inhyuk Andy Cho <cih9088@gmail.com>
2026-02-22 20:18:12 +0900

    feat(vim): no more annoying cmd suggestion
    The suggestion closes output abnormally

--- config/nvim/lua/config/lsp/nvim-cmp.lua
+++ config/nvim/lua/config/lsp/nvim-cmp.lua
@@ -132,15 +132,6 @@ local function cmp_setup()
       },
    })
 
-   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-   cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-         { name = 'path' }
-      }, {
-         { name = 'cmdline' }
-      })
-   })
 end
 
 function M.setup()
