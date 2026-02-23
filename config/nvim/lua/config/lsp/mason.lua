fcfd7d6fe9c9cd32a51e2115efd09e3919c2df6f
Inhyuk Andy Cho <cih9088@gmail.com>
2026-02-23 19:03:16 +0900

    fix(vim): fix tree-sitter-cli version for glib2.27

--- config/nvim/lua/config/lsp/mason.lua
+++ config/nvim/lua/config/lsp/mason.lua
@@ -74,7 +74,9 @@ function M.setup()
         "delve",
         "bash-debug-adapter",
 
-        "tree-sitter-cli",
+        -- https://github.com/tree-sitter/tree-sitter/issues/4174
+        -- glibc 2.27 compatible
+        "tree-sitter-cli@v0.25.10",
       },
     })
   end
